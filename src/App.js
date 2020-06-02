0fa6ebb820c0c21b60b93f9129d87c6516131f18
faridelya <60794908+faridelya@users.noreply.github.com>
2020-06-02 23:36:14 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    farid hello and welcome to react
+    </div>
+  );
+}
+
+export default App;
